caef84afa2e91b60c2ebd19c09c73e028f9208fa
Iren Mercado Salazar <93907053+iren-salazar@users.noreply.github.com>
2023-06-28 21:42:54 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import Index from './components/Index.js';
+import Navbar from './components/Navbar.js';
+
+export default function App() {
+  return (
+      <div>
+        <Navbar/>
+        <Index/>
+      </div>
+    )
+}
+
+
+
+
